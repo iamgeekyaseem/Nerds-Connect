45fb436211c36fd5fdcb1be1b0d5eaa4c8188bef
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-07-10 16:13:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000; //bydefault webs run on port 80 in live server
+
+app.listen(port, function(err){
+    if(err){
+        // console.log('Error', err);
+        //using interpolation which means instead of joining strsings or vraibles we can embedd variable in string using 'backtick'
+        console.log(`Error in running the server: ${err}`)
+    }
+    console.log(`Server is running on port: ${port}`);
+})
