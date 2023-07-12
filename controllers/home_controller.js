25088f43d0a091bcd760d2e208792bfe16887a78
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-07-12 23:51:34 +0530

    added models for sign up/sign in using mongoose in models folder

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    res.end('<h1>express controller is running<h1/>');
+    // res.end('<h1>express controller is running<h1/>');
+    res.render('home', {
+        title: "Home"
+    });
 }
---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamp: true
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.export = User;
