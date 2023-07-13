480e8bf8e206288b7bbffc1581ac7146b798587f
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-07-13 18:17:18 +0530

    added sign in sign up pages and todo their action in the users_controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,14 +2,26 @@ module.exports.profile = function(req, res){
     res.end('<h1>User profile</h1>');
 }
 
+//render sign in page
 module.exports.signin = function(req, res){
     res.render('user_sign_in',{
         title: "Sign-In Page"
     });
 }
 
+//render signup page
 module.exports.signup = function(req, res){
     res.render('user_sign_up',{
-        title: "Sign-Un Page"
+        title: "Sign-Up Page"
     });
+}
+
+//get the signup data
+module.exports.create = function(req, res){
+    //toDo later
+}
+
+//sign in and create a session for user
+module.exports.createSession = function(req, res){
+    //todo later
 }
