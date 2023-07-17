4c2a2ca0d992ad60b946c25d081b0a79a5cc3151
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-07-17 16:42:37 +0530

    saving the session cookie and storing it in mongo-connect db so that when server restart the user is not logged out

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport_local_strategy');
+// const MongoStore = require('connect-mongo')(session); //for version 3
+const MongoStore = require('connect-mongo'); //for version 4
 
 const expressLayouts = require('express-ejs-layouts');
 
@@ -29,6 +31,8 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+//mongo store is used to store the session cookie in db
 app.use(session({
     name: 'nerdsConnect',
     //TODO change the secret before deployment in production
@@ -37,7 +41,17 @@ app.use(session({
     resave: false,
     cookie:{
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            // mongooseConnection: db,
+            mongoUrl: 'mongodb://127.0.0.1/nerdsConnect_development',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongo db setup-ok');
+        }
+    )
 }))
 
 app.use(passport.initialize());
