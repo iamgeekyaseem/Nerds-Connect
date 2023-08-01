2218a8f6353e3ee7be9878e08e492206ab3dd302
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-08-01 19:11:10 +0530

    Add delete post button with authentication

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -12,5 +13,22 @@ module.exports.create = function(req, res){
             return;
         }
         
+    })
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id)
+    .then(post => {
+        //.id means converting the _id object into string
+        if (post.user == req.user.id){
+            post.deleteOne({});
+
+            Comment.deleteMany({post: req.params.id})
+            .then(err => {
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
     })
 }
