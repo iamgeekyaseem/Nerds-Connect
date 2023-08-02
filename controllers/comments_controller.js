6d3a7e1d52972cb1e9a24a93c88c9e7188162953
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-08-02 14:25:11 +0530

    Add delete comment button

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,7 +2,8 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 module.exports.create = function(req, res){
-    Post.findById(req.body.post).then(post => {
+    Post.findById(req.body.post)
+    .then(post => {
         if(post){
             Comment.create({
                 content: req.body.content,
@@ -16,4 +17,24 @@ module.exports.create = function(req, res){
             });
         }
     });
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id)
+    .then(comment  => {
+        //TODO: Add a logic so that post user can delete any comments as well
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+            comment.deleteOne();
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id} })
+            .then(post => {
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+
+
 }
