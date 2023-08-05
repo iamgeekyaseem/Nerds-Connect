d63e13bf4f673a82bee8388b5b623a6730e75eb3
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-08-05 12:20:59 +0530

    Added async awiat in controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,30 +1,31 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res) {
-    // res.end('<h1>express controller is running<h1/>');
+module.exports.home = async function (req, res) {
 
-    // reading the cookies sent by browser using cookie parser installed
-    // console.log(req.cookies);
-    // // changing the value of cookie
-    // res.cookie('user_id', 15)
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments', //derived from the array of comment present in post
-        populate: {
-            path: 'user'
-        }
-    })
-    .then(posts => {
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments', //derived from the array of comment present in post
+                populate: {
+                    path: 'user'
+                }
+            });
 
-        User.find({})
-        .then(user => {
-            res.render('home', {
-                title: "nerdsConnect | Home",
-                posts: posts,
-                all_users: user 
-            })
-        })
-    });
+        let users = await User.find({});
+
+        res.render('home', {
+            title: "nerdsConnect | Home",
+            posts: posts,
+            all_users: users
+        });
+
+    } catch (err) {
+        console.log('Error', err);
+        return;
+    }
+
+
+
 };
