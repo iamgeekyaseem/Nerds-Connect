0465e56c9a40c6abcf1d9134ecf55f4536cb176d
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-08-14 11:12:34 +0530

    Add flash noty to config passport_local for invalid user/pass noti and ERROR: After wrong pass noti shows fine but then error is thrown as 'Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client' #ToFix

--- config/passport_local_strategy.js
+++ config/passport_local_strategy.js
@@ -7,16 +7,17 @@ const User = require('../models/user');
 //authentication useing passport
 passport.use(new LocalStrategy({
     //username filed in schema
-    usernameField: 'email'
+    usernameField: 'email',
+    passReqToCallback: true
 },
-    function (email, password, done) {
+    function (req, email, password, done) {
         //find a user and establish the identity
         //first email is the property in user.js schema and value is email that is passes to the function
         User.findOne({ email: email })
             .then(user => {
                 if (!user || user.password !== password) {
-                    console.log('Invalid username/password');
-                    return Promise.reject('Invalid username/password');
+                    req.flash('error', 'Invalid username/password');
+                    return done(null, false);
                 }
                 return Promise.resolve(user);
             })
@@ -24,7 +25,7 @@ passport.use(new LocalStrategy({
                 return done(null, user);
             })
             .catch(err => {
-                console.log('Error in finding user ---> Passport', err);
+                req.flash('error', err);
                 return done(err);
             })
     }
@@ -41,27 +42,27 @@ passport.serializeUser(function (user, done) {
 //deserializinf  the user from the key in the cookies
 passport.deserializeUser(function (id, done) {
     User.findById(id)
-    .then(user => {
-        return done(null, user);
-      })
-      .catch(err => {
-        console.log('Error in finding user ---> Passport', err);
-        return done(null);
-      });
+        .then(user => {
+            return done(null, user);
+        })
+        .catch(err => {
+            console.log('Error in finding user ---> Passport', err);
+            return done(null);
+        });
 });
 
 //check is the user is authenticated
-passport.checkAuthentication = function(req, res, next){
+passport.checkAuthentication = function (req, res, next) {
     //if user is signed in, then pass on the request to the next function(conreoller's action)
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return next();
     }
     //if the user is not signed in
     return res.redirect('/users/sign-in')
 }
 
-passport.setAuthenticatedUser = function(req, res, next){
-    if(req.isAuthenticated()){
+passport.setAuthenticatedUser = function (req, res, next) {
+    if (req.isAuthenticated()) {
         //req.user contains the current signed in user from the session cookie and we are just sending it to the locals for views
         res.locals.user = req.user;
     }
